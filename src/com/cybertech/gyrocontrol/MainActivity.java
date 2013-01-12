e58169e91378d95ce2bbf5d699dfdf73c65d1f36
iakov <jake.kirilenko@gmail.com>
2013-01-12 11:54:53 +0400

    Simple initial app

--- src/com/cybertech/gyrocontrol/MainActivity.java
+++ src/com/cybertech/gyrocontrol/MainActivity.java
@@ -1,15 +1,39 @@
 package com.cybertech.gyrocontrol;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener {
+	TextView x; 
+	TextView y; 
+	TextView z;
+	private SensorManager sensorManager;
+	
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        x=(TextView)findViewById(R.id.x);
+		y=(TextView)findViewById(R.id.y);
+		z=(TextView)findViewById(R.id.z);
+		sensorManager=(SensorManager)getSystemService(SENSOR_SERVICE);
+		sensorManager.registerListener(this, 
+				sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+				SensorManager.SENSOR_DELAY_FASTEST);
+		
+		/*	More sensor speeds (taken from api docs)
+		    SENSOR_DELAY_FASTEST get sensor data as fast as possible
+		    SENSOR_DELAY_GAME	rate suitable for games
+		 	SENSOR_DELAY_NORMAL	rate (default) suitable for screen orientation changes
+		*/
     }
 
     @Override
@@ -18,5 +42,26 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
-    
+
+	@Override
+	public void onAccuracyChanged(Sensor arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		// check sensor type
+		if(event.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
+			
+			// assign directions
+			float x=event.values[0];
+			float y=event.values[1];
+			float z=event.values[2];
+			
+			this.x.setText("X: "+x);
+			this.y.setText("Y: "+y);
+			this.z.setText("Z: "+z);
+		}
+	}
 }
