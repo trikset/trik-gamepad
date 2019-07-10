81530f818446e68aa85668d3234077a430b650a3
Ilya Balashov <ibalashov24@gmail.com>
2019-07-10 14:23:45 +0300

    Union SDK and Android versions into one line

--- as/src/main/java/com/trikset/gamepad/SettingsActivity.java
+++ as/src/main/java/com/trikset/gamepad/SettingsActivity.java
@@ -21,24 +21,25 @@ public class SettingsActivity extends PreferenceActivity {
     public static final String SK_WHEEL_STEP   = "wheelSens";
 
     public static final String SK_ABOUT_FIRMWARE        = "firmwareVersion";
-    public static final String SK_ABOUT_ANDROID         = "androidVersion";
-    public static final String SK_ABOUT_GRAPHIC_MODE    = "graphicMode";
+    public static final String SK_ABOUT_GRAPHICS_MODE   = "graphicsMode";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.pref_general);
+
         final Preference firmwareVersion = findPreference(SK_ABOUT_FIRMWARE);
-        final Preference androidVersion = findPreference(SK_ABOUT_ANDROID);
-        final Preference graphicalMode = findPreference(SK_ABOUT_GRAPHIC_MODE);
+        firmwareVersion.setSummary(String.format(
+                Locale.ENGLISH,
+                "Android %s (SDK %d)",
+                Build.VERSION.RELEASE,
+                Build.VERSION.SDK_INT));
 
-        firmwareVersion.setSummary(Integer.toString(Build.VERSION.SDK_INT));
-        androidVersion.setSummary(Build.VERSION.RELEASE);
-
+        final Preference graphicsMode = findPreference(SK_ABOUT_GRAPHICS_MODE);
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        graphicalMode.setSummary(String.format(
+        graphicsMode.setSummary(String.format(
                 Locale.ENGLISH,
                 "%d x %d (%f x %f PPI)",
                 displayMetrics.heightPixels,
